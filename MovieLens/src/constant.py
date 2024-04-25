f3a5939e81d6a8bb02085959be4a508049250f79
Jiaqi Yin <jiaqiyin@yahoo.com>
2024-04-25 01:12:35 -0700

    study movielen data

---
+++ MovieLens/src/constant.py
@@ -0,0 +1,31 @@
+# define constant variables
+
+OCCUPATION_MAP = {
+    0: "other",
+    1: "academic/educator",
+    2: "artist",
+    3: "clerical/admin",
+    4: "college/grad student",
+    5: "customer service",
+    6: "doctor/health care",
+    7: "executive/managerial",
+    8: "farmer",
+    9: "homemaker",
+    10: "K-12 student",
+    11: "lawyer",
+    12: "programmer",
+    13: "retired",
+    14: "sales/marketing",
+    15: "scientist",
+    16: "self-employed",
+    17: "technician/engineer",
+    18: "tradesman/engineer",
+    19: "unemployed",
+    20: "writer"
+}
+
+RATING_COLUMN_NAMES = ['user_id', 'movie_id', 'rating', 'timestamp']
+MOVIE_COLUMN_NAMES = ['movie_id', 'title', 'genres']
+USER_COLUMN_NAMES = ['user_id', 'gender', 'age', 'occupation', 'zip_code']
+
+
